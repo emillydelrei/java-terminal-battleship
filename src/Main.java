fb527b615f322742b77854aed9b0bd6b7d317a23
Emilly D'el Rei <emillydelrei07@gmail.com>
2026-06-13 23:32:43 -0300

    Corrige bug: adiciona validação de range para input do usuário

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         java.util.Random randomGeneration = new java.util.Random();
         Scanner scanner = new Scanner(System.in);
-        int[] sea = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] sea = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int tamanhomar = sea.length - 3;
         int posicaoInicial = randomGeneration.nextInt(tamanhomar);
         int[] posicao = {posicaoInicial + 1, posicaoInicial + 2, posicaoInicial};
@@ -28,6 +28,12 @@ public class Main {
                 String guess = scanner.nextLine();
                 int intGuess = Integer.parseInt(guess);
 
+
+                if (intGuess < 0 || intGuess > 9) {
+                    System.out.println("Digite um número entre 0 e " + sea.length);
+                    continue;
+                }
+
                 String result = myShip.checkYourself(intGuess);
                 System.out.println(result);
                 numGuess++;
@@ -39,8 +45,8 @@ public class Main {
                 System.out.println("Digite apenas números!");
                 continue;
             }
-
         }
+        scanner.close();
 
 
     }
